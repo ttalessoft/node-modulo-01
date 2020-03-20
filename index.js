a4551a02eefecb5ea6ce9495f084aca730e2ed0b
ttales gold roger <ttalessoft@gmail.com>
2020-03-20 18:47:49 -0300

    configurando o nodemon

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 
-var app = express();
+var server = express();
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+// Query params = ?firstName=joao
+// Route params = /users/1
+// Request body = { "name": "João", "email":"joao@emaildele.com.br"}
+
+const users = ['joaozinho', 'luizinho', 'maria', 'mariana', 'bruna'];
+
+server.get('/users/:index', function(req, res) {
+  const { index } = req.params;
+  if (users[index]) {
+    return res.json(users[index]);
+  } else {
+    return res.json({
+      message: 'Pessoa não encontrada!'
+    });
+  }
 });
 
-app.listen(3000, function() {
-  console.log('teste');
-});
+server.listen(3000);

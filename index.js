16326f48d276ec8ad6f23ddddc8683066223416b
ttales gold roger <ttalessoft@gmail.com>
2020-03-20 19:55:57 -0300

    removendo node_modules do github

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ var express = require('express');
 
 var server = express();
 
+server.use(express.json());
+
 // Query params = ?firstName=joao
 // Route params = /users/1
 // Request body = { "name": "João", "email":"joao@emaildele.com.br"}
 
 const users = ['joaozinho', 'luizinho', 'maria', 'mariana', 'bruna'];
 
+// midlleware
+server.use((req, res, next) => {
+  console.log(`Método: ${req.method}; URL: ${req.url}`);
+  return next();
+});
+
+// listall
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+// search user by id
 server.get('/users/:index', function(req, res) {
   const { index } = req.params;
   if (users[index]) {
@@ -17,6 +31,28 @@ server.get('/users/:index', function(req, res) {
       message: 'Pessoa não encontrada!'
     });
   }
+});
+
+// save user
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+// edit user
+server.put('/user/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+  users[index] = name;
+  return res.json(users);
+});
+
+// delete user
+server.delete('/user/:index', (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+  return res.send();
 });
 
 server.listen(3000);
